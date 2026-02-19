af846d6ee179a6b78589e47d031d111bd8d1a56a
Ali1180-uni <thegreat1552005@gmail.com>
2026-02-19 21:10:11 +0500

    Comments Removed

--- Lab 2/oop in python.py
+++ Lab 2/oop in python.py
@@ -1,15 +1,15 @@
 import turtle as t
 # <---------- Basic Task ---------->
 
-# class dog:
-#     attr1 = "mammal"
-#     attr2 = "dog"
-#     def fun(self):
-#         print("i am a", self.attr1)
-#         print("i am a", self.attr2)
-# Roger = dog()
-# print(Roger.attr1)
-# Roger.fun()
+class dog:
+    attr1 = "mammal"
+    attr2 = "dog"
+    def fun(self):
+        print("i am a", self.attr1)
+        print("i am a", self.attr2)
+Roger = dog()
+print(Roger.attr1)
+Roger.fun()
 
 # <---------- Basic Task ---------->
 
