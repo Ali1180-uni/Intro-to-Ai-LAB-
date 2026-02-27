5343948574e42e384a3ac09d24a9fbebd88b1c62
Ali1180-uni <thegreat1552005@gmail.com>
2026-02-27 23:09:11 +0500

    2 Question Added

--- Lab 3/Home Task.py
+++ Lab 3/Home Task.py
@@ -1,1 +1,83 @@
 # Question 1
+
+class Loan:
+    def __init__(self, annualInterestRate=2.5, numberOfYears=1, loanAmount=1000, borrower=""):
+        self.__annualInterestRate = annualInterestRate
+        self.__numberOfYears = numberOfYears
+        self.__loanAmount = loanAmount
+        self.__borrower = borrower
+
+    def getAnnualInterestRate(self):
+        return self.__annualInterestRate
+
+    def getNumberOfYears(self):
+        return self.__numberOfYears
+
+    def getLoanAmount(self):
+        return self.__loanAmount
+
+    def getBorrower(self):
+        return self.__borrower
+
+    def setAnnualInterestRate(self, annualInterestRate):
+        self.__annualInterestRate = annualInterestRate
+
+    def setNumberOfYears(self, numberOfYears):
+        self.__numberOfYears = numberOfYears
+
+    def setLoanAmount(self, loanAmount):
+        self.__loanAmount = loanAmount
+
+    def setBorrower(self, borrower):
+        self.__borrower = borrower
+
+    def getMonthlyPayment(self):
+        monthlyInterestRate = self.__annualInterestRate / 1200
+        monthlyPayment = self.__loanAmount * monthlyInterestRate / (1 - (1 + monthlyInterestRate) ** (self.__numberOfYears * 12))
+        return monthlyPayment
+
+    def getTotalPayment(self):
+        totalPayment = self.getMonthlyPayment() * self.__numberOfYears * 12
+        return totalPayment
+
+
+loan1 = Loan()
+loan1.setAnnualInterestRate(90)
+loan1.setNumberOfYears(1)
+print(loan1.getTotalPayment())
+
+# Question 2
+
+class BMI:
+    def __init__(self, name, age, weight, height):
+        self.__name = name
+        self.__age = age
+        self.__weight = weight
+        self.__height = height
+
+    def getBMI(self):
+        bmi = self.__weight / (self.__height ** 2)
+        return bmi
+
+    def getStatus(self):
+        bmi = self.getBMI()
+        if bmi < 18.5:
+            return "Underweight"
+        else:
+            return "Overweight"
+
+    def getName(self):
+        return self.__name
+
+    def getAge(self):
+        return self.__age
+
+    def getWeight(self):
+        return self.__weight
+
+    def getHeight(self):
+        return self.__height
+
+bmi1 = BMI("Ali", 25, 70, 1.75)
+print(bmi1.getBMI())
+print(bmi1.getStatus())
