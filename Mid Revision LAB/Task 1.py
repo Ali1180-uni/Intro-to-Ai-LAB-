01b1c74e794b45e09489f7fdafac2fe91c982429
Ali1180-uni <thegreat1552005@gmail.com>
2026-03-26 11:20:59 +0500

    Revision Lab Added

--- Mid Revision LAB/Task 1.py
+++ Mid Revision LAB/Task 1.py
@@ -1,3 +1,4 @@
+import turtle as t
 # Question 1
 # x = input("Enter a Degree in Celsius: ")
 # fahrenheit = (9/5) * float(x) + 32
@@ -27,3 +28,21 @@
 # print(f"Net Pay: {net_pay}")
 
 # Question 4
+# amount = float(input("Enter investment amount: "))
+# annual_rate = float(input("Enter annual interest rate: "))
+# years = float(input("Enter number of years: "))
+# future_value = amount * (1 + annual_rate / 1200) ** (years * 12)
+# print(f"Accumulated value is {future_value}")
+
+# Question 5
+# x = input("Enter point x: ")
+# y = input("Enter point y: ")
+# width = 10
+# height = 5
+# r = 10
+# circle = float(x)**2 + float(y)**2 < r**2
+# print(f"Point ({x}, {y}) is {'inside the circle' if circle else 'outside the circle'}")
+# rectangle = abs(float(x)) <= width/2 and abs(float(y)) <= height/2
+# print(f"Point ({x}, {y}) is {'inside the rectangle' if rectangle else 'outside the rectangle'}")
+
+# Question 6
