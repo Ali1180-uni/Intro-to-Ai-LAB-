c7017df6315cae71611b14f01b512328645bcf33
Ali1180-uni <thegreat1552005@gmail.com>
2026-03-26 11:02:22 +0500

    Revision Lab Added

---
+++ Mid Revision LAB/Task 1.py
@@ -0,0 +1,29 @@
+# Question 1
+# x = input("Enter a Degree in Celsius: ")
+# fahrenheit = (9/5) * float(x) + 32
+# print(f"The {x} Celsius is {fahrenheit} Fahrenheit.")
+
+# Question 2
+# r,l = input("Enter the radius and length of a cylinder: ").split()
+# area = float(r)**2 * 3.14
+# volume = area * float(l)
+# print(f"The area is {area}")
+# print(f"The volume is {volume}")
+
+# Question 3
+# name = input("Enter your name: ")
+# hours = input("Enter hours you work: ")
+# pay = input("Enter your hourly pay rate: ")
+# f_tax = input("Enter your federal tax rate: ")
+# s_tax = input("Enter your state tax rate: ")
+# gross_pay = float(hours) * float(pay)
+# f_tax_amount = gross_pay * float(f_tax)
+# s_tax_amount = gross_pay * float(s_tax)
+# net_pay = gross_pay - f_tax_amount - s_tax_amount
+# print(f"Employee Name: {name}")
+# print(f"Gross Pay: {gross_pay}")
+# print(f"Federal Withholding (20%): {f_tax_amount}")
+# print(f"State Tax (9%): {s_tax_amount}")
+# print(f"Net Pay: {net_pay}")
+
+# Question 4
