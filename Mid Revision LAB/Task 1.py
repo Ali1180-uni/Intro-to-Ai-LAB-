10e65d89e4ffd7eb440dfe6aaf5b444668502c9c
Ali1180-uni <thegreat1552005@gmail.com>
2026-04-09 10:47:50 +0500

    Changes in Lab

--- Mid Revision LAB/Task 1.py
+++ Mid Revision LAB/Task 1.py
@@ -1,37 +1,39 @@
+# 24-NTU-CS-FL-1180 (All Rights Reserved)
+
 # Question 1
-# x = input("Enter a Degree in Celsius: ")
-# fahrenheit = (9/5) * float(x) + 32
-# print(f"The {x} Celsius is {fahrenheit} Fahrenheit.")
+x = input("Enter a Degree in Celsius: ")
+fahrenheit = (9/5) * float(x) + 32
+print(f"The {x} Celsius is {fahrenheit} Fahrenheit.")
 
 # Question 2
-# r,l = input("Enter the radius and length of a cylinder: ").split()
-# area = float(r)**2 * 3.14
-# volume = area * float(l)
-# print(f"The area is {area}")
-# print(f"The volume is {volume}")
+r,l = input("Enter the radius and length of a cylinder: ").split()
+area = float(r)**2 * 3.14
+volume = area * float(l)
+print(f"The area is {area}")
+print(f"The volume is {volume}")
 
 # Question 3
-# name = input("Enter your name: ")
-# hours = input("Enter hours you work: ")
-# pay = input("Enter your hourly pay rate: ")
-# f_tax = input("Enter your federal tax rate: ")
-# s_tax = input("Enter your state tax rate: ")
-# gross_pay = float(hours) * float(pay)
-# f_tax_amount = gross_pay * float(f_tax)
-# s_tax_amount = gross_pay * float(s_tax)
-# net_pay = gross_pay - f_tax_amount - s_tax_amount
-# print(f"Employee Name: {name}")
-# print(f"Gross Pay: {gross_pay}")
-# print(f"Federal Withholding (20%): {f_tax_amount}")
-# print(f"State Tax (9%): {s_tax_amount}")
-# print(f"Net Pay: {net_pay}")
+name = input("Enter your name: ")
+hours = input("Enter hours you work: ")
+pay = input("Enter your hourly pay rate: ")
+f_tax = input("Enter your federal tax rate: ")
+s_tax = input("Enter your state tax rate: ")
+gross_pay = float(hours) * float(pay)
+f_tax_amount = gross_pay * float(f_tax)
+s_tax_amount = gross_pay * float(s_tax)
+net_pay = gross_pay - f_tax_amount - s_tax_amount
+print(f"Employee Name: {name}")
+print(f"Gross Pay: {gross_pay}")
+print(f"Federal Withholding (20%): {f_tax_amount}")
+print(f"State Tax (9%): {s_tax_amount}")
+print(f"Net Pay: {net_pay}")
 
 # Question 4
-# amount = float(input("Enter investment amount: "))
-# annual_rate = float(input("Enter annual interest rate: "))
-# years = float(input("Enter number of years: "))
-# future_value = amount * (1 + annual_rate / 1200) ** (years * 12)
-# print(f"Accumulated value is {future_value}")
+amount = float(input("Enter investment amount: "))
+annual_rate = float(input("Enter annual interest rate: "))
+years = float(input("Enter number of years: "))
+future_value = amount * (1 + annual_rate / 1200) ** (years * 12)
+print(f"Accumulated value is {future_value}")
 
 # Question 5
 # x = input("Enter point x: ")
